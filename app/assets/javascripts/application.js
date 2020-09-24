39ad0dd76e4e5f6f9acdbb559789f31afeb99c82
zinahalnajjar <59237103+zinahalnajjar@users.noreply.github.com>
2020-09-23 23:03:57 -0400

    updated restrict views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,16 +19,8 @@
 
 
 $(document).on('turbolinks:load', function() {
-
-  $('.ui.dropdown')
-  .dropdown();
-  $('.message .close')
-  .on('click', function() {
-    $(this)
-      .closest('.message')
-      .transition('fade')
-    ;
-  })
-;
-
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
