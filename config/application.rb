d2fd44d36b2036e35824adc91b55ea3e4f0926a9
Zinah Al-Najjar <zeena.alnajjar98@gmail.com>
2024-07-01 23:14:29 -0400

    updates versions on gemfile

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module MessageMeApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.active_support.cache_format_version = 7.1
   end
 end
